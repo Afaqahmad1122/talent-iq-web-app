2171f009fc4b479d64ab2ab5a8401138e7267efc
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-11-07 13:24:30 +0500

    Add package.json and update backend server with new routes and environment variable

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,30 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
 
 const PORT = ENV.PORT;
 
+const __dirname = path.resolve();
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
+
+app.get("/books", (req, res) => {
+  res.status(200).json({ message: "Books are running" });
+});
+
+// make ready for deployment
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, ",,.frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
